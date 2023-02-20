14e159fb8f1726b7f303ebd72f38b6958cd9324b
evertonmessias <everton.messias@gmail.com>
2023-02-20 11:54:06 -0300

    20-02-2023_11:54:06

--- wp-content/themes/daesys/404.php
+++ wp-content/themes/daesys/404.php
@@ -4,9 +4,8 @@
 
     <section class="section error-404 min-vh-100 d-flex flex-column align-items-center justify-content-center">
       <h1>404</h1>
-      <h2>The page you are looking for doesn't exist.</h2>
-      <a class="btn" href="/">Back to home</a>
-      <img src="<?php echo SITEPATH; ?>assets/img/not-found.svg" class="img-fluid py-5" alt="Page Not Found">
+      <h2>Página não encontrada!</h2> 
+      <a class="btn" href="/">Voltar</a>
     </section>
 
   </div>
